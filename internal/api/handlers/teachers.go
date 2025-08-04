873b20d19b110f1ad313a0e003a5ba530d7c7504
Aravind <aravindshenoy@ideaclan.com>
2025-08-04 10:27:17 +0530

    Added generic filter function and removed filtering based on only first and last name.

--- internal/api/handlers/teachers.go
+++ internal/api/handlers/teachers.go
@@ -78,20 +78,11 @@ func getTeachersHandler(w http.ResponseWriter, r *http.Request) {
 	idStr := strings.TrimSuffix(path, "/")
 
 	if idStr == "" {
-		firstName := r.URL.Query().Get("first_name")
-		lastName := r.URL.Query().Get("last_name")
 		
 		query := "SELECT id, first_name, last_name, email, class, subject FROM teachers WHERE 1=1"
 		var args []interface{}
 
-		if firstName != ""{
-			query+=" AND first_name = ?"
-			args = append(args, firstName)
-		}
-				if lastName != ""{
-			query+=" AND last_name = ?"
-			args = append(args, lastName)
-		}
+		query, args = addFilters(r, query, args)
 		
 		rows, err := db.Query(query, args...)
 		if err != nil {
@@ -144,6 +135,25 @@ func getTeachersHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(teacher)
 
+}
+
+func addFilters(r *http.Request, query string, args []interface{}) (string, []interface{}) {
+	params := map[string]string{
+		"first_name": "first_name",
+		"last_name":  "last_name",
+		"email":      "email",
+		"class":      "class",
+		"subject":    "subject",
+	}
+
+	for param, dbField := range params {
+		value := r.URL.Query().Get(param)
+		if value != "" {
+			query += " AND " + dbField + " = ?"
+			args = append(args, value)
+		}
+	}
+	return query, args
 }
 
 func addTeacherHandler(w http.ResponseWriter, r *http.Request) {
